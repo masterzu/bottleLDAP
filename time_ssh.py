7650b822be6ec26cbdb40bfe99007bf4a2fa06ef
Master ZU <master.h2zu@gmail.com>
2016-09-22 15:34:44 +0200

    update time test

--- time_ssh.py
+++ time_ssh.py
@@ -1,5 +1,17 @@
-import paramiko, Crypto
+import paramiko
+try:
+    import Crypto
+except:
+    pass
+try:
+    import cryptography
+except:
+    pass
+
 import os
+
+
+times = []
 
 # timeit decorator
 def timeit(method):
@@ -18,7 +30,8 @@ def timeit(method):
         result = method(*args, **kw)
         te = time.time()
 
-        print '%r (%r, %r) %2.2f sec' % (method.__name__, args, kw, te-ts)
+        # print '%r (%r, %r) %2.2f sec' % (method.__name__, args, kw, te-ts)
+        times.append((method.__name__,te-ts))
         return result
 
     return timed
@@ -73,13 +86,46 @@ def ssh_commands(ssh, list_cmds):
     return list_out
 
 
-print '-------------------'
-print "Module paramiko %s" % paramiko.__version__
-print "Module pycrypto %s" % Crypto.__version__
-print '-------------------'
-print ''
+def print_long():
+    print '-------------------'
+    print "Module paramiko %s" % paramiko.__version__
+    try:
+        print "Module pycrypto %s" % Crypto.__version__
+    except:
+        pass
+    try:
+        print "Module cryptography %s" % cryptography.__version__
+    except:
+        pass
+    print '-------------------'
+    print ''
+
+    for obj in times:
+        print "%s: %.2f" % obj
+
+def print_short():
+    def _p(t): 
+        return "%.2fs" % t[1]
+
+    print "paramiko(%s)" % paramiko.__version__,
+    try:
+        print "pycrypto(%s)" % Crypto.__version__,
+    except:
+        pass
+    try:
+        print "cryptography(%s)" % cryptography.__version__
+    except:
+        pass
+    print
+    print ' '.join(map(_p, times))
+
+
+# main 
+
 c = ssh_init()
 ssh_connect(c,'olympe')
 ssh_commands(c, ['uname'])
-
 c.close()
+
+#print_long()
+print_short()
